0090f46f3c346650ecfef68c339035d539c7f02f
Chad Tindel <ctindel@Chads-MacBook-Pro-2.local>
2015-03-08 21:46:47 -0400

    Added tests for marking entries unread

---
+++ api/v1.0/test/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+node setup_tests.js
+jasmine-node create_accounts_error_spec.js
+jasmine-node create_accounts_spec.js
+node write_creds.js
+jasmine-node subscribe_spec.js
+jasmine-node feed_err_spec.js
+jasmine-node feed_spec.js
+jasmine-node feed_entry_read_spec.js
+jasmine-node feed_read_spec.js
+jasmine.node feed_entry_unread_spec.js
